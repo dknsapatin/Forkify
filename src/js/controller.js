9d24ead75c535ff873f69138ff2e0382844e1163
dknsapatin <dknsapatin@gmail.com>
2022-03-07 22:37:05 -0500

    Started on my first API Call | Installed parcel as my script

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,39 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+// Async function named showRecipe | Once called, it will fetch for the API in the background --Convert the response into JSON and save as a data
+const showRecipe = async function () {
+  try {
+    // Fetch for the API
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    // Convert constant "res" to JSON | Save it to constant "data"
+    const data = await res.json();
+
+    // Throw new error message base off of what the console says in each object
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    // Changing underscore objects with "."
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+
+    console.log(res, data);
+    // Catch if there are any errors and display them if it does.
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
